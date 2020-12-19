c45582135c00e17d1e11b680e9194b5d82ea104b
Song Shao Ting <qwe820610@gmail.com>
2020-12-19 15:17:28 +0800

    feat:modify app.js and add create.handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const exphbs = require('express-handlebars')
 const express = require('express')
 const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant.js')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
@@ -22,7 +23,24 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+// 建立餐廳內容
+app.get('/create', (req, res) => res.render('create'))
+
+app.post('/createList', (req, res) => {
+  if (!req.body.restaurantImage) {
+    req.body.image = 'https://via.placeholder.com/600x300.png?text=Restaurants'
+  }
+  const restaurant = req.body
+  console.log(restaurant)
+  return Restaurant.create(restaurant)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
