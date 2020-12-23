949822f1517a66d655d788445b8bfa50492ada6a
Song Shao Ting <qwe820610@gmail.com>
2020-12-23 16:09:14 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant.js')
+const handlebars = require('handlebars')
+// register helper
+handlebars.registerHelper('sortMethod', function (type, selectedType, options) {
+  return (type === selectedType) ? options.fn(this) : options.inverse(this)
+})
+
 // 首頁
 router.get('/', (req, res) => {
+  const type = req.query.sorting
+  const sortMethod = {
+    ratingAsc: { rating: 'desc' },
+    timeAsc: { _id: 'asc' },
+    timeDesc: { _id: 'desc' },
+    nameAsc: { name: 'asc' },
+    nameDesc: { name: 'desc' }
+  }
   Restaurant.find()
     .lean()
-    .then(restaurant => res.render('index', { restaurant }))
+    .sort(sortMethod[type])
+    .then(restaurant => res.render('index', { restaurant, type }))
     .catch(error => console.error(error))
 })
 
