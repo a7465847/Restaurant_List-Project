22f1c8026a8e7c90f7420f42e208cae9ee4f9c92
Song Shao Ting <qwe820610@gmail.com>
2021-01-14 09:19:04 +0800

    feat: add user register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,7 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
-  return res.render('login', { userCSS: true })
+  return res.render('login', { login: true })
+})
+
+router.get('/register', (req, res) => {
+  return res.render('register', { register: true })
+})
+
+router.post('/register', (req, res) => {
+  // const { name, email, password, confirmPassword } = req.body
+  console.log(req.body)
 })
 
 module.exports = router
