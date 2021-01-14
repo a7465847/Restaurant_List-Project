41e088c3c991eef2f57bdff208ae3375fe479378
Song Shao Ting <qwe820610@gmail.com>
2021-01-14 10:49:27 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const exphbs = require('express-handlebars')
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
@@ -11,6 +12,12 @@ const port = 3000
 
 app.engine('hbs', exphbs({ helpers: comparison, defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false, // true 強制與使用者互動後 session to add session store
+  saveUninitialized: true // 登入前先追蹤使用者
+}))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
