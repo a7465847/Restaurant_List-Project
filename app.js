10a20c093f10b3bd70b8acbfc17f38f1ef3c926d
Song Shao Ting <qwe820610@gmail.com>
2021-01-14 19:46:52 +0800

    Revert "feat: add isAuthenticated to display different view templates"
    This reverts commit 9fa8ba8363dbd8c54cee7c8d02288cb56fba05ef.

--- app.js
+++ app.js
@@ -26,13 +26,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.user = req.user
-  next()
-})
-
 app.use(routes)
 
 app.listen(port, () => {
