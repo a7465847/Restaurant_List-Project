39936a2458709799be4215b95ac727ffbf03ba78
Song Shao Ting <qwe820610@gmail.com>
2021-01-15 15:10:03 +0800

    feat: modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,12 +4,15 @@ const Restaurant = require('../../models/restaurant.js')
 
 // 搜尋
 router.get('/', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword
-  Restaurant.find()
+  Restaurant.find({ userId: { $eq: userId } })
     .lean()
     .then(restaurant => {
       const searchRestaurant = restaurant.filter(restaurant => {
-        return restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.trim().toLowerCase())
+        return restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase()) ||
+        restaurant.category.toLowerCase().includes(keyword.trim().toLowerCase()) ||
+        restaurant.name_en.toLowerCase().includes(keyword.trim().toLowerCase())
       })
       res.render('index', { restaurant: searchRestaurant, keyword: keyword })
     })
